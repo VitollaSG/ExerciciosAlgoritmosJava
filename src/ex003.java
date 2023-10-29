1400f216c08a14b5043db0ec99438e7b4bf20c7b
Victor Souza <victorsouza.fsa@hotmail.com>
2023-10-29 17:34:32 -0300

    .

---
+++ src/ex003.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ex003 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Qual nome do funcionário? ");
+        String nome = sc.nextLine();
+
+        System.out.print("Qual o valor do salário? ");
+        double sal = sc.nextDouble();
+
+        System.out.printf ("O funcionário %s tem um slário de R$ %.2f reais", nome, sal);
+    }
+}
