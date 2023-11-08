2da53bfb4181143775d5f4de7253f2bc59918796
Victor Souza <victorsouza.fsa@hotmail.com>
2023-11-08 17:24:15 -0300

    ex061 e ex062

---
+++ src/ex061.java
@@ -0,0 +1,13 @@
+public class ex061 {
+    public static void main(String[] args) {
+
+        int n = 0;
+        do{
+            System.out.print(n + " ");
+            n = n + 3;
+
+        }while (n <= 30);
+        System.out.printf("acabou!!");
+
+    }
+}
---
+++ src/ex062.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ex062 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int Nidade = 0; int totalIdade = 0; int Midade = 0; 
+        String cont = "";
+
+        do {
+            System.out.print("Digite a idade: ");
+            int idade = sc.nextInt();
+
+            totalIdade += idade;
+            Nidade ++;
+
+            if (idade > 21){
+                Midade++;
+            }
+
+            System.out.print("Quer continuar? [S/N]");
+            cont = sc.next();
+
+        } while(cont.equalsIgnoreCase("s"));
+        System.out.printf("Foram digitadoas %d idades %n", Nidade);
+        System.out.printf("A média entre todas idades digitadas é %d %n", totalIdade/Nidade );
+        System.out.printf("%d pessoas tem mais que 21 anos %n", Midade);
+
+        sc.close();
+    }
+}
